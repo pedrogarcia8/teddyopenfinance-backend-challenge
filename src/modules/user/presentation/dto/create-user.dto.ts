30de4a241d5202231d955092e273642fc6b8df74
pedrogarcia8 <pedro.jose.garcia8@gmail.com>
2025-04-25 17:07:11 -0300

    chore/swagger: Add user documentation

--- src/modules/user/presentation/dto/create-user.dto.ts
+++ src/modules/user/presentation/dto/create-user.dto.ts
@@ -1,10 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    format: 'email',
+    example: 'test@email.com',
+    description: 'User email',
+    required: true,
+  })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: 'string',
+    example: '123456',
+    description: 'User password',
+    required: true,
+  })
   password: string;
 }
